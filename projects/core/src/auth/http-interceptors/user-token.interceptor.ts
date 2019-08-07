ecd21000d5f7574408136803dc00d8fa09e15cfa
Patrick Labadie <patrick.labadie@sap.com>
2019-08-07 13:15:13 -0400

    GH-1511: Remove console log

--- projects/core/src/auth/http-interceptors/user-token.interceptor.ts
+++ projects/core/src/auth/http-interceptors/user-token.interceptor.ts
@@ -24,7 +24,6 @@ export class UserTokenInterceptor implements HttpInterceptor {
     return this.authService.getUserToken().pipe(
       take(1),
       switchMap(token => {
-        console.log('UserTokenInterceptor url', request.url);
         if (
           token &&
           this.isOccUrl(request.url) &&
